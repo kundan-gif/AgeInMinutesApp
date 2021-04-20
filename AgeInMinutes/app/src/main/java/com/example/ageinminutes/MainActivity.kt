f30aaca217b7eb89fd3386716d40d3e88973f1a1
Kundan Kumar <kundan211097@gmail.com>
2021-04-20 23:41:41 +0530

    Calulating the age in minutes and displaying it

--- AgeInMinutes/app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ AgeInMinutes/app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,17 +20,30 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     @RequiresApi(Build.VERSION_CODES.N)
-    fun clickDatePicker(view:View){
-        val myCalendar=Calendar.getInstance()
-        val year=myCalendar.get(Calendar.YEAR)
-        val  month=myCalendar.get(Calendar.MONTH)
-        val day=myCalendar.get(Calendar.DAY_OF_MONTH)
+    fun clickDatePicker(view: View) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this,DatePickerDialog
-            .OnDateSetListener { view, year, month, dayOfMonth ->
+        val dpd=DatePickerDialog(this, DatePickerDialog
+            .OnDateSetListener
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
 
+                val selectedDate="$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+                tvSelectedDate.text=selectedDate
+                val sdf=SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate=sdf.parse(selectedDate)
+                val selectedDateInMinutes=theDate!!.time/60000
+                val currentDate=sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinutes=currentDate!!.time/60000
+                val differenceInMinutes=currentDateInMinutes-selectedDateInMinutes
+                tvSelectedDateInMinute.text=differenceInMinutes.toString()
             }
-            ,year,month,day).show()
+            , year, month, day)
+        dpd.datePicker.setMaxDate(Date().time-86400000)
+        dpd.show()
     }
 }
