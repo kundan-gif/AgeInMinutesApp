cc7599595331ae918e07cbdd5ecc6b9cc32c83b7
Kundan Kumar <kundan211097@gmail.com>
2021-04-20 22:57:19 +0530

    Opening DatePickerDialog on click of select date button

--- AgeInMinutes/app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ AgeInMinutes/app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnDatePicker.setOnClickListener {
+            clickDatePicker(it)
+        }
+
+    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickDatePicker(view:View){
+        val myCalendar=Calendar.getInstance()
+        val year=myCalendar.get(Calendar.YEAR)
+        val  month=myCalendar.get(Calendar.MONTH)
+        val day=myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,DatePickerDialog
+            .OnDateSetListener { view, year, month, dayOfMonth ->
+
+            }
+            ,year,month,day).show()
     }
 }
